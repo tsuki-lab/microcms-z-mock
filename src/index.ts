6eae0d4661eb237d2f78f144f1bc4a3143487601
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2023-02-11 22:21:55 +0900

    feat: added design memo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("HelloWorld");
+import {z} from 'zod'
+
+type MicrocmsAPI = {
+  list: { [Key: string]: unknown }
+  object: { [Key: string]: unknown }
+}
+
+const reference = <T extends unknown>(schema: T): T => {
+  // コンテンツ参照の為のなんらかの処理
+  return schema
+}
+
+export const createMock = (microcmsAPI: MicrocmsAPI) => (): void => {
+  // モックサーバーの実行処理
+}
+
+const categorySchema = z.object({
+  name: z.string().or(z.undefined())
+})
+
+const blogSchema = z.object({
+  title: z.string().or(z.undefined()),
+  content: z.string().or(z.undefined()),
+  eyecatch: z.object({
+    url: z.string().url(),
+    height: z.number(),
+    width: z.number(),
+  }),
+  category: reference(categorySchema)
+})
+
+const settingsSchema = z.object({
+  siteName: z.string()
+})
+
+const microcmsAPI = {
+  list: {
+    categories: categorySchema,
+    blogs: blogSchema
+  },
+  object: {
+    settings: settingsSchema
+  },
+}  as const satisfies MicrocmsAPI
+
+const executeMock = createMock(microcmsAPI)
+
+// モックサーバー（msw）を実行する
+executeMock()
